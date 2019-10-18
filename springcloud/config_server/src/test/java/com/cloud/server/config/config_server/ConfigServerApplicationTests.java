81c8a7c7376c62e733439c9b79c84cb55fa60e48
maoxiaoxiong <31685834+maoxiaoxiong@users.noreply.github.com>
2019-10-19 02:13:53 +0800

    Add files via upload

---
+++ springcloud/config_server/src/test/java/com/cloud/server/config/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.server.config.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springcloud/eureka_server/src/main/java/com/cloud/eureka/eureka_cloud/EurekaCloudApplication.java
@@ -0,0 +1,15 @@
+package com.cloud.eureka.eureka_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaCloudApplication.class, args);
+    }
+
+}
