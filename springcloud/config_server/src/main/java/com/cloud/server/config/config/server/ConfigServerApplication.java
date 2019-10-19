a2222db026d4b6e4621fd9f76c4bdd43f3aca64a
maoxiaoxiong <1064517852@qq.com>
2019-10-19 15:06:07 +0800

    配置中心

--- springcloud/config_server/src/main/java/com/cloud/server/config/config/server/ConfigServerApplication.java
+++ springcloud/config_server/src/main/java/com/cloud/server/config/config/server/ConfigServerApplication.java
@@ -2,8 +2,14 @@ package com.cloud.server.config.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+//开启配置中心
+@EnableConfigServer
 @SpringBootApplication
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
